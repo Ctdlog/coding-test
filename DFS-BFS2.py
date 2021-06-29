2e19d4e6406627d008ce5db8f0952d280c2bc311
Ctdlog <73215539+Choi-HW980402@users.noreply.github.com>
2021-06-29 11:09:15 +0900

    DFS-BFS2 deque

--- DFS-BFS2.py
+++ DFS-BFS2.py
@@ -5,29 +5,32 @@ for i in range(n):
     graph.append(list(map(int, input())))
 
 
-def dfs(x, y):
-    # # 주어진 범위를 벗어나면 돌아가기
-    # if x <= -1 or x >= n or y <= -1 or y >= m:
-    #     dfs(x, y)
+def dfs(x, y, result):
+    # 주어진 범위를 벗어나면 돌아가기
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        dfs(x, y, result)
+
+    if x == n - 1 and y == m - 1:
+        print(result)
+        return False
 
     # 오른쪽, 아래쪽 순으로 먼저 DFS
     if graph[x + 1][y] == 1:
         x += 1
-        dfs(x, y)
+        result += 1
+        dfs(x, y, result)
     elif graph[x][y + 1] == 1:
         y += 1
-        dfs(x, y)
-        return True
-    if x == n and y == m:
-        return False
+        result += 1
+        dfs(x, y, result)
+    elif graph[x - 1][y] == 1:
+        x -= 1
+        result += 1
+        dfs(x, y, result)
+    elif graph[x][y - 1] == 1:
+        y -= 1
+        result += 1
+        dfs(x, y, result)
 
 
-dfs(1, 1)
-result = 0
-for i in range(n):
-    for j in range(m):
-        # 현재 위치에서 DFS 수행
-        if dfs(i, j) == True:
-            result += 1
-
-print(result)
+dfs(0, 0, 1)
