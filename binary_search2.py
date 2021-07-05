af93c6bc0472d96fc9657f8fb1585559a4c79851
Ctdlog <73215539+Choi-HW980402@users.noreply.github.com>
2021-07-05 13:18:54 +0900

    Update binary_search2.py

--- binary_search2.py
+++ binary_search2.py
@@ -14,15 +14,15 @@ while start <= end:
     mid = (start + end) // 2
     for x in array:
         # 잘랐을 때의 떡의 양 계산
-            if x > mid:
-                total += x - mid
+        if x > mid:
+            total += x - mid
         # 떡의 양이 부족한 경우 더 많이 자르기(왼쪽 부분 탐색)
-        if total < m:
-            end = mid - 1
-        # 떡의 양이 충분한 경우 덜 자르기(오른쪽 부분 탐색)
-        else:
-            result = mid  # 최대한 덜 잘랐을 때가 정답이므로, 여기에서 result에 기록
-            start = mid + 1
+    if total < m:
+        end = mid - 1
+    # 떡의 양이 충분한 경우 덜 자르기(오른쪽 부분 탐색)
+    else:
+        result = mid  # 최대한 덜 잘랐을 때가 정답이므로, 여기에서 result에 기록
+        start = mid + 1
 
 # 정답 출력
 print(result)
