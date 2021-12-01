1542f8446c866838c0759b74f2cb7277e1c9a382
Ctdlog <qpflapffhs76@gmail.com>
2021-12-01 15:13:11 +0900

    인프런 JS Coding Test 1201

---
+++ infrean_js/1-5.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+  let answer,
+    min = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) min = arr[i];
+  }
+  answer = min;
+  return answer;
+}
+
+let arr = [5, 7, 1, 3, 2, 9, 11];
+console.log(solution(arr));
