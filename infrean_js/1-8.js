abdaa61311e1a917f20f8a2c82a75d22615ed232
CTD <qpflapffhs76@gmail.com>
2021-12-16 22:54:47 +0900

    Coding Test 1216

--- infrean_js/1-8.js
+++ infrean_js/1-8.js
@@ -1,17 +1,17 @@
 let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
 
 function solution(arr) {
-  let answer = [];
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  let a = sum - 100;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr.slice(i).find((v) => v == a - arr[i]) != undefined) {
-      console.log(arr.find((v) => v == a - arr[i]));
+  let answer = arr;
+  let sum = arr.reduce((a, b) => a + b, 0);
+  for (let i = 0; i < 8; i++) {
+    for (let j = i + 1; j < 9; j++) {
+      if (sum - (arr[i] + arr[j]) === 100) {
+        arr.splice(j, 1);
+        arr.splice(i, 1);
+      }
     }
   }
+
   return answer;
 }
 
