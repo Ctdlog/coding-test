2d1ce2b325dce7da5de15adb318203f08af0d9db
ctdlog <qpflapffhs76@gmail.com>
2022-03-21 22:50:07 +0900

    CodingTest #220321

--- infrean_js/section3/4/app.js
+++ infrean_js/section3/4/app.js
@@ -3,12 +3,35 @@ const filePath = "./input.txt";
 let input = fs.readFileSync(filePath).toString().split(" ");
 
 input = input[0];
-str = input[1];
+tg = input[1];
 
-console.log(input, str);
+solution(input, tg);
 
-console.log(input.charAt(str));
-
-function solution(A) {
+function solution(input, tg) {
+  let result = [];
+  let cnt = 0;
   // Write your code
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] !== tg) {
+      cnt++;
+    } else {
+      cnt = 0;
+    }
+    result.push(cnt);
+  }
+
+  cnt = 0;
+
+  for (let i = input.length - 1; i >= 0; i--) {
+    if (input[i] !== tg) {
+      cnt++;
+    } else {
+      cnt = 0;
+    }
+    if (result[i] > cnt) {
+      result.splice(i, 1);
+      result.splice(i, 0, cnt);
+    }
+  }
+  console.log(result);
 }
