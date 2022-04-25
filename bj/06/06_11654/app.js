34d7dc3b2b86a7347270d5b505947ee29dd01e2e
ctd <qpflapffhs76@gmail.com>
2022-04-25 10:03:07 +0900

    .

---
+++ bj/06/06_11654/app.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+input = input[0];
+
+solution(input);
+
+function solution(A) {
+  // Write your code
+  console.log(A.charCodeAt());
+}
