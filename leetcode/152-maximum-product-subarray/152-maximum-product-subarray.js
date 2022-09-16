7284621095a55c51a626a9c0fe5eff60d3638e32
HyungWook <qpflapffhs76@gmail.com>
2022-09-16 13:31:27 +0900

    Time: 111 ms (31.63%), Space: 42.3 MB (87.39%) - LeetHub

---
+++ leetcode/152-maximum-product-subarray/152-maximum-product-subarray.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    let currentMax = nums[0];
+	let currentMin = nums[0];
+	let result = nums[0];
+
+	for (let i = 1; i < nums.length; i++) {
+		let temp = currentMax;
+
+		currentMax = Math.max(
+			Math.max(currentMax * nums[i], currentMin * nums[i]),
+			nums[i]
+		);
+
+		currentMin = Math.min(
+			Math.min(temp * nums[i], currentMin * nums[i]),
+			nums[i]
+		);
+
+		result = Math.max(currentMax, result);
+	}
+
+	return result;
+};
