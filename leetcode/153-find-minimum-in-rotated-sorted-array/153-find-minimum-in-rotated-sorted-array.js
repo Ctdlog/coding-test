d414c753112d12a9805e2a036d422ce28f884671
HyungWook <qpflapffhs76@gmail.com>
2022-09-17 16:26:20 +0900

    Time: 104 ms (24.06%), Space: 41.9 MB (80.36%) - LeetHub

--- leetcode/153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
+++ leetcode/153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -3,6 +3,37 @@
  * @return {number}
  */
 var findMin = function(nums) {
-	const min = Math.min(...nums);
-	return min;
+	let left = 0;
+	let right = nums.length - 1;
+
+	if (nums.length === 1) {
+		return nums[0];
+	}
+
+	if (nums[left] < nums[right]) {
+		return nums[left];
+	}
+
+	while (left <= right) {
+		const mid = Math.floor((left + right) / 2);
+
+		const leftValue = nums[left];
+		const midValue = nums[mid];
+		const leftOfMid = nums[mid - 1];
+		const rightOfMid = nums[mid + 1];
+
+		if (midValue > rightOfMid) {
+			return rightOfMid;
+		}
+
+		if (leftOfMid > midValue) {
+			return midValue;
+		}
+
+		if (midValue > leftValue) {
+			left = mid + 1;
+		} else {
+			right = mid - 1;
+		}
+	}
 };
