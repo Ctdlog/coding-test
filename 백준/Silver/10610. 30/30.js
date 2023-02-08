ad192421f8b0d09bb1f37cd81789dc05d2b357f5
Fragment <qpflapffhs76@gmail.com>
2023-02-08 23:03:35 +0900

    [Silver IV] Title: 30, Time: 220 ms, Memory: 15076 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const N = input.shift().split('');
+
+let result = -1;
+
+const solution = (N) => {
+  if (!N.includes('0')) {
+    return -1;
+  }
+
+  const sum = N.reduce((acc, cur) => acc + Number(cur), 0);
+  if (sum % 3 !== 0) {
+    return -1;
+  }
+
+  return N.sort((a, b) => b - a).join('');
+};
+
+console.log(solution(N));
