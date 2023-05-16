88861b938ffe1765a66f987bdeba8d1ceb69f721
Fragment <qpflapffhs76@gmail.com>
2023-05-16 22:15:59 +0900

    [Silver V] Title: 다리 놓기, Time: 212 ms, Memory: 12196 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.js
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.js
@@ -2,15 +2,18 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-const T = +input.shift();
+const T = input.shift();
+
 const cases = input.map((i) => i.split(' ').map((item) => +item));
 
 for (const [N, M] of cases) {
   const DP = [];
+
   for (let i = 0; i < M + 1; i++) {
     let a = new Array(N + 1).fill(0);
     DP.push(a);
   }
+
   function combination(m, n) {
     if (DP[m][n] > 0) return DP[m][n];
     if (n == m || n == 0) return 1;
@@ -18,5 +21,6 @@ for (const [N, M] of cases) {
     DP[m][n] = combination(m - 1, n - 1) + combination(m - 1, n);
     return DP[m][n];
   }
+
   console.log(combination(M, N));
 }
