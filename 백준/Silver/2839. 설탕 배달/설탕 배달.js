e33d0e4e7cb4e3ed45b8a185ef8e9e9ccfe1e8d0
Fragment <qpflapffhs76@gmail.com>
2023-05-18 21:51:25 +0900

    [Silver IV] Title: 설탕 배달, Time: 132 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+input = +input[0];
+
+solution(input);
+
+function solution(n) {
+  // Write your code
+  let count = 0;
+
+  while (true) {
+    if (input % 5 === 0) {
+      console.log(input / 5 + count);
+      break;
+    }
+
+    if (0 > input) {
+      console.log(-1);
+      break;
+    }
+
+    count++;
+    input -= 3;
+  }
+}
