ffb95eabe2fc87547a05ace3d96b6c5cc8d45c35
Hyung Wook Choi <qpflapffhs76@gmail.com>
2023-08-21 21:47:08 +0900

    Time: 48 ms (89.24%), Space: 42.6 MB (25.12%) - LeetHub

---
+++ leetcode/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    let answer = "";
+
+    const max = Math.max(word1.length, word2.length);
+
+    for (let i=0; i<max; i++) {
+        if (word1[i]) {
+            answer = answer + word1[i];
+        }
+
+        if (word2[i]) {
+            answer = answer + word2[i];
+        }
+    }
+
+    return answer;
+};
