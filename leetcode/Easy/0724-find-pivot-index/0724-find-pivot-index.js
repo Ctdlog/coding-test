933353847c8a02ff803c55b0b4ea1dcb6a420622
Hyung Wook Choi <qpflapffhs76@gmail.com>
2023-09-29 02:00:51 +0900

    Time: 70 ms (35.69%), Space: 44.3 MB (65.80%) - LeetHub

---
+++ leetcode/Easy/0724-find-pivot-index/0724-find-pivot-index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let totalSum = 0;
+    let leftSum = 0;
+
+    for (let num of nums) {
+        totalSum += num;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (leftSum === totalSum - leftSum - nums[i]) {
+            return i;
+        }
+        leftSum += nums[i];
+    }
+
+    return -1;
+};
