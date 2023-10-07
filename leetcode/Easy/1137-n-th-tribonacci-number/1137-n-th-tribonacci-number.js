d366dd85ef5d73615a4bdc50636143c67b8f6f7d
Hyung Wook Choi <qpflapffhs76@gmail.com>
2023-10-08 01:18:27 +0900

    Time: 45 ms (82.56%), Space: 41.7 MB (54.07%) - LeetHub

---
+++ leetcode/Easy/1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function(n) {
+    const DP = [0, 1, 1];
+
+    for (let i = 3; i <= n; i++) {
+        DP[i] = DP[i - 1] + DP[i - 2] + DP[i - 3];
+    }
+
+    return DP[n];
+};
